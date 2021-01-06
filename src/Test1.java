beb34354e15c2099b9ceee15cc54852d3987bc61
Vaibhav30986 <Vnarkhede@eagle.org>
2021-01-06 17:40:54 +0530

    Modified by Vaibhav after installing tortise

--- src/Test1.java
+++ src/Test1.java
@@ -3,12 +3,13 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Nik");
+		System.out.println("Nik - modified by Vaibhav");
 
 		//
 
-		//Modified By Nilesh
+		//Modified By Nilesh - Modified by Vaibhav
 
+		
 		System.out.println("Vaibhav made changes");
 		
 		System.out.println("Second day - Vaibhav Made changes");
@@ -20,7 +21,8 @@ public class Test1 {
 		System.out.println("Second day - Vaibhav Made changes");
 		System.out.println("Second day - Vaibhav Made changes");
 		
-		//
+		//After Installing GitTortise
+		System.out.println("Vaibhav made changes ");
 
 	}
 
